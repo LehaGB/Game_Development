0a213a945ef18466a611037cca78f20da2a4866e
Алексей <manay.ru@mail.ru>
2026-07-02 10:44:24 +1000

    2D: Случайный размер , направление и скорость движения

---
+++ Sprite Flight/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    [SerializeField] private float minSize = 0.5f;
+    [SerializeField] private float maxSize = 1.8f;
+
+    [SerializeField] private float minSpeed = 50f;
+    [SerializeField] private float maxSpeed = 150f;
+
+    [SerializeField] private float maxSpinSped = 10f;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        float randomSize = Random.Range(minSize, maxSize);
+        transform.localScale = new Vector3(randomSize, randomSize, 1f);
+
+        float randomSpeed = Random.Range(minSpeed, maxSpeed) / randomSize;
+
+        rb = GetComponent<Rigidbody2D>();
+
+        Vector2 randomDirection = Random.insideUnitCircle;
+        rb.AddForce(randomDirection * randomSpeed);
+
+        float randomTorque = Random.Range(-maxSpinSped, maxSpinSped) / randomSize;
+        rb.AddTorque(randomTorque * Time.deltaTime);
+    }
+}
